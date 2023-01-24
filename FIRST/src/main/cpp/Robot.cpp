1fa79e6281e840b620fc23e36673ad10ad69e8df
jeremiahjy <jeremiahjy@madisoncity.k12.al.us>
2023-01-23 22:28:32 -0600

    Adjust initial motor direction

--- FIRST/src/main/cpp/Robot.cpp
+++ FIRST/src/main/cpp/Robot.cpp
@@ -14,6 +14,7 @@ void Robot::RobotInit() {
 
   left_front.SetInverted(false);
   right_front.SetInverted(false);*/
+  left_front.SetInverted(true);
   left_drive.SetInverted(false);
   right_drive.SetInverted(false);
 }
