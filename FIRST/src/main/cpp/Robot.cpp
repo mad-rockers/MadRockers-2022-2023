67e8d596479dd1d00c7b0e831984b56efe2b440d
jeremiahjy <jereyang2017@gmail.com>
2023-04-10 17:52:26 -0500

    Add comments to auto

--- FIRST/src/main/cpp/Robot.cpp
+++ FIRST/src/main/cpp/Robot.cpp
@@ -58,9 +58,11 @@ void Robot::RobotPeriodic() {
   SmartDashboard::PutNumber("Gyro Rate", double(r_gyro.GetRate()));
 }
 
+//This code only runs once at the beginning of autonomous
 void Robot::AutonomousInit() {
+  //Begin by resetting and rezeroing variables and encoders
   stop_all();
-  auto_state = 0;
+  auto_state = 0; //This variable here controls what step in the auto code to run. It starts at 0.
   left_hold = 0;
   r_left_encoder.SetPosition(0);
   r_right_encoder.SetPosition(0);
@@ -69,7 +71,12 @@ void Robot::AutonomousInit() {
   timer1.Reset();
 }
 
+//This code runs in an infinite loop all throughout autonomous
 void Robot::AutonomousPeriodic() {
+  /*
+  This section is just dedicated to initializing some variables for how the auto code will run.
+  The benefit of putting the settings up here is that they are easy and fast to tweak.
+  */
   double arm_place;
   double extension_place;
   double initial_back;
@@ -89,23 +96,65 @@ void Robot::AutonomousPeriodic() {
   double balance_speed = 0.1;
   units::degree_t balance_zone = 3_deg;
   double balance_rate = 5;
+
+  /*
+  This switch statement is where the actual auto runs.
+  Depending on what auto_state is, it will only run 1 of the steps.
+  Each step is under a "case" statement.
+  So only one of the sections under a case statement will be running at a time.
+
+  Step descriptions:
+  0 - Pull up the extension and drive back a little. If grabbing a cone, grab with high pressure.
+  1 - Raise the arm
+  2 - Move the extension out
+  3 - If placing a cone, move the arm down a little bit.
+  4 - Open the grabber
+  5 - Drive back a set amount (either onto the charge station or out of the community).
+      While this is happening, pull back the extension and lower the arm.
+  6 - If going on the charge station, run the balancing routine.
+  */
   switch(auto_state) {
     case 0:
+      /*
+      1 of the 2 ways that a motor can be run is through its PID controller.
+      This is useful for setting a motor to go to an exact position.
+      In the statement below, you can see that I have the extension motor set to go to the 0 position by using its PID controller.
+      The SetReference() command will run the motor to a specified number of MOTOR rotations from the 0 point.
+      */
       r_extension_pid.SetReference(0, ControlType::kPosition);
+
+      /*
+      We had 2 auto routines set for competition.
+      There was a charge station auto and a non charge station auto.
+      The way I told the robot which auto to run was by putting a selector in Shuffleboard.
+      r_auto_mode is the selector and I can get which option is selected by using the GetSelected() command.
+      */
       if(r_auto_mode.GetSelected() == "No Charge Station") {
         grabber_close_high();
       }
+
+      /*
+      The second way that a motor can be run is by directly setting it to a power with the Set() command.
+      The command takes a power from -1 to 1.
+      */
       r_left_front.Set(drive_speed);
       r_right_front.Set(drive_speed);
       if(r_left_encoder.GetPosition() >= initial_back) {
         r_left_front.Set(0);
         r_right_front.Set(0);
+
+        /*
+        At some point in the auto step is a trigger to go to the next step.
+        After auto_state is incremented, this case statement will no longer run with each loop and instead the next one will run.*/
         auto_state++;
       }
       break;
     
     case 1:
       r_arm_pid.SetReference(arm_place, ControlType::kPosition);
+
+      /*If I need to get the position that a motor is currently in, I can do so by using its built-in encoder.
+      The GetPosition() command returns how many rotations the MOTOR has rotated from the 0 point.*/
       if(abs(r_arm_encoder.GetPosition() - arm_place) < 2) {
         auto_state++;
       }
